63b97e258dc3607309aa84a6f37f1adc5c7a267b
Ohoud Alajmi <ofalajmi22@twq.idserve.net>
2022-10-27 12:56:01 +0300

    Initial Commit

---
+++ simpleApp/simpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleAppApp.swift
+//  simpleApp
+//
+//  Created by Ohoudaj on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct simpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
